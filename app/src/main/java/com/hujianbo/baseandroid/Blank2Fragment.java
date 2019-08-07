60251af977648a450e4364c5dc758eca532a8941
tldxdy <747417048@qq.com>
2019-08-07 10:36:03 +0800

    修改分支

--- app/src/main/java/com/hujianbo/baseandroid/Blank2Fragment.java
+++ app/src/main/java/com/hujianbo/baseandroid/Blank2Fragment.java
@@ -31,7 +31,7 @@ public class Blank2Fragment extends BaseFragment {
     List<String> list;
     @Override
     protected void initView() {
-        LogUtils.e("initView");
+
     }
 
     @Override
